abf8987ea453fc64de5e7387bae9c0db5ae3eaee
danielambda <daniel.gevorgyan25@gmail.com>
2025-04-26 23:02:11 +0300

    feat: concreted client ot logic

--- src/main/scala/quote/Client.scala
+++ src/main/scala/quote/Client.scala
@@ -1,61 +1,74 @@
-import scala.util.{Either, Left, Right}
+import quote.ot.*
 
-sealed trait ClientState[+Op]
-case object ClientSynchronized extends ClientState[Nothing]
-case class ClientWaiting[Op](op: Op) extends ClientState[Op]
-case class ClientWaitingWithBuffer[Op](waitingOp: Op, bufferOp: Op) extends ClientState[Op]
+sealed trait ClientState
+case object ClientSynchronized extends ClientState
+case class ClientWaiting(ops: List[Operation]) extends ClientState
+case class ClientWaitingWithBuffer(
+  waitingOp: List[Operation],
+  bufferOp:  List[Operation]
+) extends ClientState
 
 object ClientState {
-  def initial[Op]: ClientState[Op] = ClientSynchronized
+  def initial: ClientState = ClientSynchronized
 }
 
-trait OTComposableOperation[Op] {
-  def compose(a: Op, b: Op): Op
-  def transform(a: Op, b: Op): (Op, Op)
-}
+object Client {
+  def applyClient(state: ClientState, op: Operation): (Boolean, ClientState) =
+    state match {
+      case ClientSynchronized =>
+        (true, ClientWaiting(List(op)))
 
-class Client[Op](implicit ot: OTComposableOperation[Op]) {
-  
-  def applyClient(state: ClientState[Op], op: Op): (Boolean, ClientState[Op]) = {
-    state match {
-      case ClientSynchronized => 
-        (true, ClientWaiting(op))
-        
-      case ClientWaiting(w) => 
-        (false, ClientWaitingWithBuffer(w, op))
-        
-      case ClientWaitingWithBuffer(w, b) =>
-        val b1 = ot.compose(b, op)
-        (false, ClientWaitingWithBuffer(w, b1))
-    }
-  }
-  
-  def applyServer(state: ClientState[Op], serverOp: Op): (Op, ClientState[Op]) = {
-    state match {
-      case ClientSynchronized => 
-        (serverOp, ClientSynchronized)
-        
       case ClientWaiting(w) =>
-        val (w1, serverOp1) = ot.transform(w, serverOp)
-        (serverOp1, ClientWaiting(w1))
-        
+        (false, ClientWaitingWithBuffer(w, List(op)))
+
       case ClientWaitingWithBuffer(w, b) =>
-        val (w1, serverOp1) = ot.transform(w, serverOp)
-        val (b1, serverOp2) = ot.transform(b, serverOp1)
-        (serverOp2, ClientWaitingWithBuffer(w1, b1))
+        (false, ClientWaitingWithBuffer(w, op :: b))
     }
-  }
-  
-  def serverAck(state: ClientState[Op]): (Option[Op], ClientState[Op]) = {
+
+  def applyServer(state: ClientState, serverOps: List[Operation]): (List[Operation], ClientState) =
     state match {
-      case ClientSynchronized => 
+      case ClientSynchronized =>
+        (serverOps, ClientSynchronized)
+
+      case ClientWaiting(w) =>
+        val (w1, serverOps1) = transform(w, serverOps)
+        (serverOps1, ClientWaiting(w1))
+
+      case ClientWaitingWithBuffer(w, b) =>
+        val (w1, serverOps1) = transform(w, serverOps)
+        val (b1, serverOps2) = transform(b, serverOps1)
+        (serverOps2, ClientWaitingWithBuffer(w1, b1))
+    }
+
+  def serverAck(state: ClientState): (Option[List[Operation]], ClientState) =
+    state match {
+      case ClientSynchronized =>
         (None, ClientSynchronized)
-        
-      case ClientWaiting(_) => 
+
+      case ClientWaiting(_) =>
         (None, ClientSynchronized)
-        
-      case ClientWaitingWithBuffer(_, b) => 
+
+      case ClientWaitingWithBuffer(_, b) =>
         (Some(b), ClientWaiting(b))
     }
-  }
+
+  val transform = transformList2
+
+  def transformList1(o: Operation, ps: List[Operation]): (Operation, List[Operation]) =
+    ps match {
+      case Nil => (o, Nil)
+      case p :: psTail =>
+        val (o1, p1) = OperationalTransformation.transform(o, p)
+        val (o2, ps1) = transformList1(o1, psTail)
+        (o2, p1 :: ps1)
+    }
+
+  def transformList2(os: List[Operation], ps: List[Operation]): (List[Operation], List[Operation]) =
+    os match {
+      case Nil => (Nil, ps)
+      case o :: osTail =>
+        val (o1,  ps1) = transformList1(o, ps)
+        val (os1, ps2) = transformList2(osTail, ps1)
+        (o1 :: os1, ps2)
+    }
 }
