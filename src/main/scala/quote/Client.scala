c4aa6cef8810b1e7d04623fd8514a33d99eacaf1
acecution <acecution7@gmail.com>
2025-04-26 19:39:07 +0300

    fix: client logic

--- src/main/scala/quote/Client.scala
+++ src/main/scala/quote/Client.scala
@@ -1,12 +1,6 @@
 import scala.util.{Either, Left, Right}
 
-sealed trait ClientState[Op] {
-  def isSynchronized: Boolean = this match {
-    case ClientSynchronized => true
-    case _ => false
-  }
-}
-
+sealed trait ClientState[+Op]
 case object ClientSynchronized extends ClientState[Nothing]
 case class ClientWaiting[Op](op: Op) extends ClientState[Op]
 case class ClientWaitingWithBuffer[Op](waitingOp: Op, bufferOp: Op) extends ClientState[Op]
@@ -20,8 +14,7 @@ trait OTComposableOperation[Op] {
   def transform(a: Op, b: Op): Either[String, (Op, Op)]
 }
 
-class Client[Op: OTComposableOperation] {
-  private val ot = implicitly[OTComposableOperation[Op]]
+class OTClient[Op](implicit ot: OTComposableOperation[Op]) {
   
   def applyClient(state: ClientState[Op], op: Op): Either[String, (Boolean, ClientState[Op])] = {
     state match {
@@ -32,10 +25,7 @@ class Client[Op: OTComposableOperation] {
         Right((false, ClientWaitingWithBuffer(w, op)))
         
       case ClientWaitingWithBuffer(w, b) =>
-        ot.compose(b, op) match {
-          case Left(err) => Left(s"operations couldn't be composed: $err")
-          case Right(b1) => Right((false, ClientWaitingWithBuffer(w, b1)))
-        }
+        ot.compose(b, op).map(b1 => (false, ClientWaitingWithBuffer(w, b1)))
     }
   }
   
@@ -45,29 +35,24 @@ class Client[Op: OTComposableOperation] {
         Right((serverOp, ClientSynchronized))
         
       case ClientWaiting(w) =>
-        ot.transform(w, serverOp) match {
-          case Left(err) => Left(s"transform failed: $err")
-          case Right((w1, serverOp1)) => Right((serverOp1, ClientWaiting(w1)))
+        ot.transform(w, serverOp).map { case (w1, serverOp1) => 
+          (serverOp1, ClientWaiting(w1))
         }
         
       case ClientWaitingWithBuffer(w, b) =>
-        for {
-          (w1, serverOp1) <- ot.transform(w, serverOp).left.map(err => s"transform failed: $err")
-          (b1, serverOp2) <- ot.transform(b, serverOp1).left.map(err => s"transform failed: $err")
-        } yield (serverOp2, ClientWaitingWithBuffer(w1, b1))
+        ot.transform(w, serverOp).flatMap { case (w1, serverOp1) =>
+          ot.transform(b, serverOp1).map { case (b1, serverOp2) =>
+            (serverOp2, ClientWaitingWithBuffer(w1, b1))
+          }
+        }
     }
   }
   
   def serverAck(state: ClientState[Op]): Option[(Option[Op], ClientState[Op])] = {
     state match {
-      case ClientSynchronized => 
-        None
-        
-      case ClientWaiting(_) => 
-        Some((None, ClientSynchronized))
-        
-      case ClientWaitingWithBuffer(_, b) => 
-        Some((Some(b), ClientWaiting(b)))
+      case ClientSynchronized => None
+      case ClientWaiting(_) => Some((None, ClientSynchronized))
+      case ClientWaitingWithBuffer(_, b) => Some((Some(b), ClientWaiting(b)))
     }
   }
 }
