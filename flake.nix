d0877cf7b979dfc827a115cd570da1e36fc7788f
danielambda <daniel.gevorgyan25@gmail.com>
2025-04-26 22:57:11 +0300

    chore: added flake.nix dev shell

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A basic flake with a shell";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            nixd
+            metals
+            typescript-language-server
+
+            coursier
+            zulu21 # needed for metals
+            vite
+
+            sbt-with-scala-native
+            nodejs_23
+          ];
+        };
+      }
+    );
+}
+
